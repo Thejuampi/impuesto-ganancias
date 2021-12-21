fbb7234ac8c8da416eed7e1c7542c3e03cbf99ed
Juan Lescano <juanpablo.lescano@gmail.com>
2021-12-21 11:42:38 -0300

    2020 values

--- groovy/src/test/groovy/IncomeTaxCalculatorTest.groovy
+++ groovy/src/test/groovy/IncomeTaxCalculatorTest.groovy
@@ -4,7 +4,7 @@ class IncomeTaxCalculatorSpecifications extends Specification {
 
   final IncomeTaxCalculator calculator = new IncomeTaxCalculator()
 
-  def 'Given a salary with no deductions, When calculate, Then result should be the expected' (BigDecimal salary, int expected) {
+  def '2019 values' (BigDecimal salary, int expected) {
     expect:
       calculator.calculate(salary, 2019).intValue() == expected
 
@@ -19,6 +19,23 @@ class IncomeTaxCalculatorSpecifications extends Specification {
       130_000.0 | 21125
       150_000.0 | 28125
       200_000.0 | 45625
+  }
+
+  def '2020 values' (BigDecimal salary, int expected) {
+    expect:
+    calculator.calculate(salary, 2020).intValue() == expected
+
+    where:
+    salary    | expected
+    10_000.0  | 0
+    30_000.0  | 0
+    50_000.0  | 0
+    70_000.0  | 153
+    90_000.0  | 2971
+    110_000.0 | 8290
+    130_000.0 | 15189
+    150_000.0 | 22189
+    200_000.0 | 39689
   }
 
 }
